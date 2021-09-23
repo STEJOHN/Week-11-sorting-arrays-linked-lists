eb30f5b723996337f23fe05a0d197344871aaaa8
Stephen Johnson <stephenjayyy@gmail.com>
2021-09-22 19:14:34 -0500

    Inital commit

---
+++ LinkedListsTraining/LinkedListsTraining/Sort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsTraining
+{
+    public static class Sort
+    {
+        public static void Bubble(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length -1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }                                                       
+        }
+
+     
+      
+
+        public static void Selection(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+        }
+
+        public static void Insertion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var temp = arr[i];
+                var j = i - 1;
+
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }  
+        }
+
+        public static void MergeSort(int[] arr)
+        {
+           
+            }
+        }
+    }
+
